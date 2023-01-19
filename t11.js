8a5b207fbbfebe5f289cad73610fd6ba911302aa
Jasur Abbasov <jabbasov13@gmail.com>
2023-01-19 17:39:18 +0400

    t11-t15

---
+++ t11.js
@@ -0,0 +1,10 @@
+// Verilən ədədin sadə ədəd olub olmadığını yoxlayan funksiya yazın. Sadə ədəd sadəcə özünə və 1-ə bölünən ədədlərə deyilir.
+
+function isPrime(num) {
+    if(num/num==1 && num/1==1){
+        return num;
+    }
+    // Sizin kodunuz
+}
+
+console.log(isPrime(2)); // true
